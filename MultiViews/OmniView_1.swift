dbf4f876ce6066a280dd90d30c21324f47280b13
george <george@georges-MacBook-Pro-2.local>
2017-08-14 08:17:09 +1000

    axis highlight progress

--- MultiViews/OmniView_1.swift
+++ MultiViews/OmniView_1.swift
@@ -88,20 +88,63 @@ class OmniView_1: NSView {
         let xDivisions:Int = 24  //15 degrees each
         
         
-        var axisPath = NSBezierPath()
+        let gridPath = NSBezierPath() //grid and axis lines
 
+        //x horizontal lines
         for i in 0...yDivisions {
-        axisPath.move(to: NSPoint(x:self.bounds.origin.x, y: (self.bounds.size.height / CGFloat(yDivisions)) * CGFloat(i) ) )
-        
-        axisPath.line(to: NSPoint(x: self.bounds.origin.x + self.bounds.width, y: (self.bounds.size.height / CGFloat(yDivisions)) * CGFloat(i) ) )
+        gridPath.move(to: NSPoint(x:self.bounds.origin.x, y: (self.bounds.height / CGFloat(yDivisions)) * CGFloat(i) ) )
+            
+        gridPath.line(to: NSPoint(x: self.bounds.origin.x + self.bounds.width, y: (self.bounds.size.height / CGFloat(yDivisions)) * CGFloat(i) ) )
         
         }
-        //let color = NSColor(calibratedHue: 0.8, saturation: 0.8, brightness: 0.5, alpha: 1)
-        let color:NSColor = NSColor(white: 1.0, alpha: 0.8)
-        color.setStroke()
         
+        //y vertical lines
+        for i in 0...xDivisions {
+        
+            gridPath.move(to: NSPoint(x: ((self.bounds.width / CGFloat(xDivisions)) * CGFloat(i)) , y: self.bounds.origin.y))
+            
+            gridPath.line(to: NSPoint(x: ((self.bounds.width / CGFloat(xDivisions)) * CGFloat(i)) , y: self.bounds.origin.y + self.bounds.size.height ))
+            
+            let test1:NSPoint = NSPoint(x: ((self.bounds.origin.x / CGFloat(xDivisions)) * CGFloat(i)) , y: self.bounds.origin.y)
+            let test2:NSPoint = NSPoint(x: ((self.bounds.origin.x / CGFloat(xDivisions)) * CGFloat(i)) , y: self.bounds.origin.y + self.bounds.height )
+            
+        //Swift.print(" line from \(test1) to \(test2) i is \(i)")
+            
+        }
+        
+        // stroke them
+        var gridColor = NSColor(calibratedHue: 0.8, saturation: 0.8, brightness: 0.1, alpha: 0.4)
+        //let gridColor:NSColor = NSColor(white: 1.0, alpha: 0.8)
+        gridColor.setStroke()
+        gridPath.lineWidth = 0.5
+        gridPath.stroke()
+        
+        // now axis highlights
+        
+        let axisPath = NSBezierPath() //grid and axis lines
+        axisPath.move(to: NSPoint(x: self.bounds.origin.x  , y: self.bounds.origin.y))
+        
+        axisPath.line(to: NSPoint(x: self.bounds.origin.x , y: self.bounds.origin.y + self.bounds.size.height ))
+        
+        var i = 0
+        for i in stride(from: 0, to: yDivisions, by: 5) {
+            
+           axisPath.move(to: NSPoint(x:self.bounds.origin.x, y: (self.bounds.height / CGFloat(yDivisions)) * CGFloat(i) + CGFloat(self.bounds.height / CGFloat(yDivisions)))  )
+            
+            axisPath.line(to: NSPoint(x: self.bounds.origin.x + self.bounds.width, y: (self.bounds.size.height / CGFloat(yDivisions)) * CGFloat(i) + CGFloat(self.bounds.height / CGFloat(yDivisions)))  )
+            
+        }
+        
+        
+        
+        
+
+        let axisColor = NSColor(calibratedHue: 0.8, saturation: 0.8, brightness: 0.1, alpha: 1)
+        axisColor.setStroke()
         axisPath.lineWidth = 0.5
         axisPath.stroke()
+
+        
         
         
         
@@ -112,17 +155,17 @@ class OmniView_1: NSView {
 //        
 //        let axisEndPoint: CGPoint = CGPoint(x:self.bounds.origin.x + self.bounds.width , y: self.bounds.size.height /
 //        
-//        let axisPath = CGMutablePath()
+//        let gridPath = CGMutablePath()
 //        
-//        axisPath.move(to: axisStartPoint)
+//        gridPath.move(to: axisStartPoint)
 //        
-//        axisPath.addLine(to: axisEndPoint )
+//        gridPath.addLine(to: axisEndPoint )
 //        
 //        (axisContext)!.saveGState()
 //        let axisColor1: NSColor = NSColor(hue: 0.0, saturation: 0.0, brightness: 0.0, alpha: 1)
 //        axisContext?.setLineWidth(0.5)
 //        axisContext?.setStrokeColor(axisColor1.cgColor)
-//        axisContext?.addPath(axisPath)
+//        axisContext?.addPath(gridPath)
 //        axisContext?.strokePath()
 //        (axisContext)!.restoreGState()
 //
