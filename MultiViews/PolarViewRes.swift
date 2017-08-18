396a51e4d85da041cf0d20ae28d406de036dc0ec
george <george@georges-MacBook-Pro-2.local>
2017-08-18 16:33:49 +1000

    stage 1 complete ...omni,bi, resultant ...cartesian and polar , adjustable orientation angle

---
+++ MultiViews/PolarViewRes.swift
@@ -0,0 +1,73 @@
+//
+//  PolarViewRes.swift
+//  MultiViews
+//
+//  Created by george on 18/8/17.
+//  Copyright © 2017 george. All rights reserved.
+//
+
+import Cocoa
+
+
+@IBDesignable
+
+class PolarViewRes: NSView {
+
+    //var startAngle:Float = omni.micOrientationAngle.degreesToRadians
+    
+    
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        
+        let centreOfView: CGPoint = CGPoint(x:self.bounds.width / 2 , y: self.bounds.height / 2)
+        
+        var resultantArray: Array = [Float](repeating: 0.0, count: 360)
+        
+        for i in 0...359{
+            
+            resultantArray[i] = omni.sensitivityValues[i] + biDirectional.sensitivityValues [i]
+            
+        }
+        
+        let resultantPath = NSBezierPath()
+        
+        let startResultantArrayValue = CGFloat((resultantArray [0]) as Float)
+        let firstPoint = NSPoint(x: CGFloat(CGFloat(scaleFactor) * startResultantArrayValue ) * 0 + centreOfView.x  , y: CGFloat(CGFloat(scaleFactor) * startResultantArrayValue ) * 1 + centreOfView.y)
+        
+        
+        resultantPath.move(to: firstPoint)
+        
+       
+        
+        
+        for i in stride(from: 1, through: 359, by: 1){
+            
+            let radianValue: Float = i.degreesToRadians
+            
+            let thisResultantArrayValue = CGFloat(abs((resultantArray [i]) as Float))
+            
+            let nextPoint = NSPoint(x: CGFloat(CGFloat(scaleFactor) * thisResultantArrayValue ) * CGFloat(sin(radianValue)) + centreOfView.x  , y: CGFloat(CGFloat(scaleFactor) * thisResultantArrayValue ) * CGFloat(cos(radianValue)) + centreOfView.y )
+            
+            resultantPath.line(to: nextPoint)
+            
+            // Swift.print(" for point \(i) -> \(nextPoint)")
+            // Swift.print("------------cos radian value \(CGFloat(cos(radianValue)))")
+            // Swift.print("------------this omniArrayValue \(thisOmniArrayValue)")
+        }
+        
+        let resultantlColor = NSColor(calibratedHue: 0.1, saturation: 0.9, brightness: 0.8, alpha: 1)
+        
+        resultantlColor.setStroke()
+        resultantPath.lineWidth = 2
+        resultantPath.stroke()
+        
+        // round the view corners
+        self.layer?.cornerRadius = 15
+        
+        
+        
+        // Drawing code here.
+    }
+    
+}
--- MultiViews/ViewController.swift
+++ MultiViews/ViewController.swift
@@ -9,14 +9,19 @@
 import Cocoa
 
  //Create PolarPattern instances - omni, bidirectional, resultant
- var omni: PolarPattern = PolarPattern(pressureOp: 0.2, pressureGrad: 0.8, gain: 0.5, orientation: 0)
- var biDirectional: PolarPattern = PolarPattern(pressureOp: 0.5, pressureGrad: 0.5, gain: 0.5, orientation: 0)
+ var omni: PolarPattern = PolarPattern(pressureOp: 0.0, pressureGrad: 1.0, gain: 0.5, orientation: 0)
+ var biDirectional: PolarPattern = PolarPattern(pressureOp: 0.0, pressureGrad: 1.0, gain: 0.5, orientation: 0)
 
 class ViewController: NSViewController {
     @IBOutlet weak var cartesianView: CartesianView!
+    @IBOutlet weak var cartesianViewBi: cartesianViewBi!
     @IBOutlet weak var cartesianViewRes: CartesianViewRes!
+    
 
     @IBOutlet weak var polarView: PolarView!
+    @IBOutlet weak var polarViewBi: PolarViewBi!
+    @IBOutlet weak var polarViewRes: PolarViewRes!
+    
     
        override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,10 +45,7 @@ class ViewController: NSViewController {
         cartesianView.setNeedsDisplay(cartesianView.bounds) //see print statement in draw rect
     }
     
-
-    
-    @IBAction func viewOneLineEnd(_ sender: NSSlider) {
-        
+    @IBAction func mic_1_orientation(_ sender: NSSlider) {
         omni.micOrientationAngle = Int(sender.floatValue)
         
         let slice1: ArraySlice<Float> = omni.rawSensitivityValues [omni.micOrientationAngle ... 359]
@@ -51,17 +53,38 @@ class ViewController: NSViewController {
         
         
         omni.sensitivityValues.removeAll(keepingCapacity: true)
-         omni.sensitivityValues = Array(slice1 + slice2)
+        omni.sensitivityValues = Array(slice1 + slice2)
         omni.sensitivityValues = omni.sensitivityValues.map { $0 * omni.micGain }
-
+        
         cartesianView.setNeedsDisplay(cartesianView.bounds)
         polarView.setNeedsDisplay(polarView.bounds)
         cartesianViewRes.setNeedsDisplay(cartesianViewRes.bounds)
+        polarViewRes.setNeedsDisplay(polarViewRes.bounds)
+    
+    }
+
+    @IBAction func mic_2_orientation(_ sender: NSSlider) {
         
-//Swift.print(omni.sensitivityValues[0])
-//Swift.print(" omni orientation slider = \(sender.floatValue)")
-//Swift.print(omni.sensitivityValues[0])
-//.print(" omni.micOrientationAngle is \(omni.micOrientationAngle)")
-            }
+            biDirectional.micOrientationAngle = Int(sender.floatValue)
+        
+            let slice1: ArraySlice<Float> = biDirectional.rawSensitivityValues [biDirectional.micOrientationAngle ... 359]
+            let slice2: ArraySlice<Float> = biDirectional.rawSensitivityValues [ 0 ..< biDirectional.micOrientationAngle]
+        
+        
+            biDirectional.sensitivityValues.removeAll(keepingCapacity: true)
+            biDirectional.sensitivityValues = Array(slice1 + slice2)
+            biDirectional.sensitivityValues = biDirectional.sensitivityValues.map { $0 * biDirectional.micGain }
+        
+            cartesianViewBi.setNeedsDisplay(cartesianView.bounds)
+            polarViewBi.setNeedsDisplay(polarView.bounds)
+            cartesianViewRes.setNeedsDisplay(cartesianViewRes.bounds)
+            polarViewRes.setNeedsDisplay(polarViewRes.bounds)
+    }
+ 
+
+    
+    
+    
 }
+
 
