92132820d13b609e3f40a3206158559f32685bda
george <george@georges-MacBook-Pro-2.local>
2017-08-23 09:32:09 +1000

    wed 23/8/2017  tip

--- MultiViews/ViewController.swift
+++ MultiViews/ViewController.swift
@@ -15,6 +15,14 @@ var radioButtonPressed: Bool = true
  var biDirectional: PolarPattern = PolarPattern(pressureOp: 0.0, pressureGrad: 1.0, gain: 0.5, orientation: 0)
 
 class ViewController: NSViewController {
+    
+    
+    @IBOutlet weak var omniSlideOutlet: NSSlider!
+    @IBOutlet weak var omniCellOutlet: NSSliderCell!
+    
+    
+    
+    
     @IBOutlet weak var cartesianView: CartesianView!
     @IBOutlet weak var cartesianViewBi: cartesianViewBi!
     @IBOutlet weak var cartesianViewRes: CartesianViewRes!
@@ -221,41 +229,5 @@ class ViewController: NSViewController {
   
     
 }
-
-//@IBAction func mic_1_orientation(_ sender: NSSlider) {
-//    
-//    omni.micOrientationAngle = Int(sender.floatValue)
-//    
-//    let slice1: ArraySlice<Float> = omni.rawSensitivityValues [omni.micOrientationAngle ... 359]
-//    let slice2: ArraySlice<Float> = omni.rawSensitivityValues [ 0 ..< omni.micOrientationAngle]
-//    
-//    
-//    omni.sensitivityValues.removeAll(keepingCapacity: true)
-//    omni.sensitivityValues = Array(slice1 + slice2)
-//    omni.sensitivityValues = omni.sensitivityValues.map { $0 * omni.micGain }
-//    
-//    cartesianView.setNeedsDisplay(cartesianView.bounds)
-//    polarView.setNeedsDisplay(polarView.bounds)
-//    cartesianViewRes.setNeedsDisplay(cartesianViewRes.bounds)
-//    polarViewRes.setNeedsDisplay(polarViewRes.bounds)
-//}
-
-//@IBAction func mic_2_orientation(_ sender: NSSlider) {
-//    
-//    biDirectional.micOrientationAngle = Int(sender.floatValue)
-//    
-//    let slice1: ArraySlice<Float> = biDirectional.rawSensitivityValues [biDirectional.micOrientationAngle ... 359]
-//    let slice2: ArraySlice<Float> = biDirectional.rawSensitivityValues [ 0 ..< biDirectional.micOrientationAngle]
-//    
-//    
-//    biDirectional.sensitivityValues.removeAll(keepingCapacity: true)
-//    biDirectional.sensitivityValues = Array(slice1 + slice2)
-//    biDirectional.sensitivityValues = biDirectional.sensitivityValues.map { $0 * biDirectional.micGain }
-//    
-//    cartesianViewBi.setNeedsDisplay(cartesianView.bounds)
-//    polarViewBi.setNeedsDisplay(polarView.bounds)
-//    cartesianViewRes.setNeedsDisplay(cartesianViewRes.bounds)
-//    polarViewRes.setNeedsDisplay(polarViewRes.bounds)
-//}
 
 
