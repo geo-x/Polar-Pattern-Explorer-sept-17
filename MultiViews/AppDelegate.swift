1e4aaa7a461923a7fc7cad0f157b6744fd11bff2
george <george@georges-MacBook-Pro-2.local>
2017-09-02 08:45:30 +1000

    show hide progress

--- MultiViews/AppDelegate.swift
+++ MultiViews/AppDelegate.swift
@@ -10,14 +10,15 @@ import Cocoa
 
 
 
-//x-x-x-x-X  Globals  X-x-x-x-x-x-x-x-x-x
-//x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+//******************** Globals ********************
+//*************************************************
 
 var testColor: CGColor = CGColor(red: 0.2, green: 0.5, blue: 0.8, alpha: 0.08) //background for graphs
 
 
-//-x-x-x-X    Global Colors   X-x-x-x-x-x-x-x
-//-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+//************************************************
+//*******************  Global Colors *************
+
 
 let gridColor = NSColor(calibratedHue: 0.8, saturation: 0.8, brightness: 0.1, alpha: 0.3)
 let axisColor = NSColor(calibratedHue: 0.8, saturation: 0.8, brightness: 0.1, alpha: 0.7)
@@ -26,6 +27,27 @@ let biDirectionalColor = NSColor(calibratedHue: 0.8, saturation: 0.9, brightness
 let resultantlColor = NSColor(hue: 0.1167, saturation: 1, brightness: 0.96, alpha: 1.0) /* #f4ab00 */
 let cursorColor = NSColor(hue: 0.975, saturation: 1, brightness: 0.95, alpha: 1.0) /* #f20024 */
 let pointerColor = NSColor(hue: 0.975, saturation: 1, brightness: 0.95, alpha: 0.5) /* #f20024 */
+
+
+//************************************************
+//*********  Show Hide Rect Colors ***************
+
+let omniRectHiStroke = NSColor(hue: 0.3861, saturation: 1, brightness: 0.96, alpha: 1.0)
+let omniRectHiFill = NSColor(hue: 0.3861, saturation: 1, brightness: 0.96, alpha: 1.0)
+let omniRectLoStroke = NSColor(hue: 0.3861, saturation: 1, brightness: 0.96, alpha: 1.0)
+let omniRectLoFill = NSColor(hue: 0.3861, saturation: 1, brightness: 0.96, alpha: 0.1)
+//
+//let biRectHiStroke = NSColor
+//let biRectHiFill = NSColor
+//let biRectLoStroke = NSColor
+//let biRectLoFill = NSColor
+//
+//let resRectHiStroke = NSColor
+//let resRectHiFill = NSColor
+//let resRectLoStroke = NSColor
+//let resRectLoFill = NSColor
+
+
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
--- MultiViews/ShowHideView.swift
+++ MultiViews/ShowHideView.swift
@@ -9,11 +9,34 @@
 import Cocoa
 
 class ShowHideView: NSView {
+    
+    
+
 
     override func draw(_ dirtyRect: NSRect) {
-        super.draw(dirtyRect)
+    
+        drawSelectionBoxes()
+        
+      
 
         // Drawing code here.
     }
+    
+}
+
+func drawSelectionBoxes() {
+    
+    let omniRect = NSBezierPath()
+    let biRect = NSBezierPath()
+    let resultRect = NSBezierPath()
+    
+    
+    omniRect.appendRoundedRect((NSRect(x:30, y:200, width:15, height:15)), xRadius: 3, yRadius: 3)
+    
+    omniRectHiStroke.setStroke()
+    omniRectLoFill.setFill()
+    omniRect.lineWidth = 2.0
+    omniRect.fill()
+    omniRect.stroke()
     
 }
