3c8479235230ee82d733d917491d5ef819d223ed
george <george@georges-MacBook-Pro-2.local>
2017-09-05 08:20:07 +1000

    mouse detect working

--- MultiViews/ShowHideView.swift
+++ MultiViews/ShowHideView.swift
@@ -15,22 +15,38 @@ var omniState = 1
 var biState = 1
 
 
-let omniSwitchRect:NSRect = NSRect(x: 30, y: 200, width: 15, height: 15)
-let biSwitchRect:NSRect = NSRect(x: 30, y: 160, width: 15, height: 15)
+//let omniSwitchRect:NSRect = NSRect(x: 30, y: 200, width: 15, height: 15)
+
 
 class ShowHideView: NSView {
     
-  
+let omniSwitchRect:NSRect = NSRect(x: 30, y: 200, width: 15, height: 15)
+let biSwitchRect:NSRect = NSRect(x: 30, y: 160, width: 15, height: 15)
     
     
     override func mouseDown(with event: NSEvent) {
         Swift.print("mouse down")
         
-        if omniSwitchRect.contains(convert(event.locationInWindow, to: self)) { Swift.print("inside rect")}
-        Swift.print(convert(event.locationInWindow, to: self))
-        Swift.print(" unconverted \(event.locationInWindow)")
         
-        //convertPoint(touch.location(in: self))  /event.locationInWindow
+        //if (omniSwitchRect).contains(event.locationInWindow) { Swift.print("inside rect")} else{ Swift.print("ouside rect")} // syntax looks good but not converting location correctly
+        
+        
+        var mouseLocation = convert(event.locationInWindow, to: self)
+        if NSMouseInRect(mouseLocation, convert(omniSwitchRect, to: self.superview), true) { Swift.print("inside omni rect")} else{ Swift.print("ouside rect")}
+        
+        
+     
+//       Swift.print("-converted location\(convert(event.locationInWindow, to: self))")
+//       Swift.print("-unconverted location \(event.locationInWindow)")
+//       Swift.print("-self.frame contains? \((self.frame).contains(event.locationInWindow))")
+//       Swift.print("-self.bounds \(self.bounds)")
+//       Swift.print("-self.frame \(self.frame)")
+//       Swift.print("**************")
+//       Swift.print("- omniSwitchRect\(self.omniSwitchRect)")
+//        Swift.print("- omniSwitchRect converted\(convert(omniSwitchRect, to: self.superview))")
+       
+        
+       
         
         switch omniState {
         case 1: omniState = 0
@@ -53,7 +69,42 @@ class ShowHideView: NSView {
     override func draw(_ dirtyRect: NSRect) {
         
 
-        drawSelectionBoxes()
+        
+        omniRect.appendRoundedRect(omniSwitchRect, xRadius: 2, yRadius: 2)
+        
+        if omniState == 1{
+            omniRectHiStroke.setStroke()
+            omniRectHiFill.setFill()
+            omniRect.lineWidth = 1.0
+            omniRect.fill()
+            omniRect.stroke()}
+        else{
+            omniRectHiStroke.setStroke()
+            omniRectLoFill.setFill()
+            omniRect.lineWidth = 1.0
+            omniRect.fill()
+            omniRect.stroke()
+        }
+        
+        biRect.appendRoundedRect(biSwitchRect, xRadius: 2, yRadius: 2)
+        
+        if biState == 1{
+            biRectHiStroke.setStroke()
+            biRectHiFill.setFill()
+            biRect.lineWidth = 1.0
+            biRect.fill()
+            biRect.stroke()}
+        else{
+            
+            
+            biRectHiStroke.setStroke()
+            biRectLoFill.setFill()
+            biRect.lineWidth = 1.0
+            biRect.fill()
+            biRect.stroke()
+            
+        }
+
         
       
 
@@ -65,49 +116,6 @@ class ShowHideView: NSView {
     
 }
 
-func drawSelectionBoxes() {
-    
-    
-    Swift.print("redraw called")
-
-    
-    omniRect.appendRoundedRect(omniSwitchRect, xRadius: 2, yRadius: 2)
-    
-    if omniState == 1{
-    omniRectHiStroke.setStroke()
-    omniRectHiFill.setFill()
-    omniRect.lineWidth = 1.0
-    omniRect.fill()
-    omniRect.stroke()}
-    else{
-    omniRectHiStroke.setStroke()
-    omniRectLoFill.setFill()
-    omniRect.lineWidth = 1.0
-    omniRect.fill()
-    omniRect.stroke()
-    }
-   
-    biRect.appendRoundedRect(biSwitchRect, xRadius: 2, yRadius: 2)
-    
-    if biState == 1{
-        biRectHiStroke.setStroke()
-        biRectHiFill.setFill()
-        biRect.lineWidth = 1.0
-        biRect.fill()
-        biRect.stroke()}
-    else{
-        
-        
-        biRectHiStroke.setStroke()
-        biRectLoFill.setFill()
-        biRect.lineWidth = 1.0
-        biRect.fill()
-        biRect.stroke()
-        
-    }
-    
-    
-}
 
 
 
